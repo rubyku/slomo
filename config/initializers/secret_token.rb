89bff64f822b2f50127f6b4969d9d0463864e05d
Ruby Ku <rubyku85@gmail.com>
2013-02-08 13:41:30 -0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slomo::Application.config.secret_token = '0386b0e8076a577e8a0d06ab45e2eb0adc34d4fb4cf5f5ae46b57c4b5b1157587a3bca76f12908ca72767ec95a9e97601b34d482932ec44349ed540540dbef13'
