14e445575e4f431bc98e56fadd102b722d0d70b3
Ruby Ku <rubyku85@gmail.com>
2013-10-04 23:13:43 -0500

    moved up social media

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @presses = Press.all.reverse
     @links = Link.all
-    @screenings = Screening.all
+    @screenings = Screening.all.reverse
     @honors = Honor.all
   end
 
